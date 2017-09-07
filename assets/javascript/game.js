60bed3d854252d36c2e3a8cf3444fb85886a645c
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-09-07 01:25:35 -0700

    created 4 characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,39 +41,42 @@
  //             starWarsGame.startGame();
  //         };
 
-		var starWarsGame = {
-                     obiwan: {
+		var starWarsGame = [
+                     {
                          name: "Obi-Wan Kenobi",
-                         picture: "../images/obi-wan.jpg",
+                         picture: "assets/images/obi-wan.jpg",
                          health: 120,
                          damage: 8,
                      },
-                     lukeSkywalker: {
+                     {
                          name: "Luke Skywalker",
                          picture: "assets/images/luke-skywalker.jpg",
                          health: 100,
                          damage: 5,
                      },
-                     darthSidious: {
+                     {
                          name: "Darth Sidious",
-                         picture: "../images/darth-sidious.jpg",
+                         picture: "assets/images/darth-sidious.jpg",
                          health: 150,
                          damage: 20,
                      },
-                     darthMaul: {
+                     {
                          name: "Darth Maul",
-                         picture: "../images/darth-maul.jpg",
+                         picture: "assets/images/darth-maul.jpg",
                          health: 180,
                          damage: 25,
                      }
-                 };
+                 ]
    			
  //   $("#characters").html(starWarsGame.lukeSkywalker.name + "<img class='char-background' src='assets/images/luke-skywalker.jpg' height=200 width=100>")
+ 	for (var i = 0; i < starWarsGame.length; i++) {
+ 	
+
  	var charBtn = $("<button>");
  	charBtn.addClass("char-background");
- 	charBtn.html(starWarsGame.lukeSkywalker.name);
- 	charBtn.append("<img class='image' src='assets/images/luke-skywalker.jpg' height=100 width=200>");
- 	charBtn.append(starWarsGame.lukeSkywalker.health);
+ 	charBtn.html(starWarsGame[i].name);
+ 	charBtn.append("<img class='image' src='" + starWarsGame[i].picture +"'' height=100 width=200>");
+ 	charBtn.append(starWarsGame[i].health);
 
  	$("#characters").append(charBtn);
- 
+ }
